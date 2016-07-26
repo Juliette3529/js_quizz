d43804f24641062a750c4e507f831078bb5cfde7
Juliette3529 <juliette@saint-jalme.fr>
2016-07-26 23:34:33 +0200

    optimisation du code pour affichage question + suppression d'id et de class inutile

--- js/script.js
+++ js/script.js
@@ -5,234 +5,30 @@ $(".false").click(function () {
 });
 $(".good").click(function () {
     $(this).css("background", '#55B270');
-    score ++;
-    $(".Felicitations").html('Vous avez'+ score +'/10');
+    score ++
+    $(".Felicitations").html('Vous avez     '+ score +'/10');
+
 });
-$(".Felicitations").html('Vous avez   '+ score +'  /10 !');
+
+
+
+
+var numQuestion = 1;
+var a = "#quest";
+var b = a + numQuestion;
+
+
+$(".suivant").click(function(){
+	$(b).css("display","none");
+	numQuestion++;
+	b = a + numQuestion;
+	$(b).css("display", "block");
 });
 
-
-
-function questionSuivante1() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "block");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionSuivante2() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "block");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante3() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "block");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante4() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "block");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionSuivante5() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "block");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante6() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "block");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante7() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "block");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante8() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "block"); $("#question10").css("display", "none");
-
-}
-
-function questionSuivante9() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "block");
-
-}
-
-function questionPrecedente2() {
-    $("#question1").css("display", "block");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-
-function questionPrecedente3() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "block");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente4() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "block");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente5() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "block");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente6() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "block");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente7() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "block");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente8() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "block");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente9() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "block");
-    $("#question9").css("display", "none"); $("#question10").css("display", "none");
-
-}
-function questionPrecedente10() {
-    $("#question1").css("display", "none");
-    $("#question2").css("display", "none");
-    $("#question3").css("display", "none");
-    $("#question4").css("display", "none");
-    $("#question5").css("display", "none");
-    $("#question6").css("display", "none");
-    $("#question7").css("display", "none");
-    $("#question8").css("display", "none");
-    $("#question9").css("display", "block"); $("#question10").css("display", "none");
-}
+$(".precedent").click(function(){
+	$(b).css("display","none");
+	numQuestion--;
+	b = a + numQuestion;
+	$(b).css("display","block");
+});
+    });
